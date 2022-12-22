8817c998762e505886dd2f06edf75607f447ea00
Carlos Pessoa <carlos.henriquepessoa15@gmail.com>
2022-12-22 20:06:29 -0300

    Header estilizado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,34 @@ import './style.css';
 import Profile from '../../assets/profile-icon.svg';
 import Logo from '../../assets/logo.svg';
 import LogOut from '../../assets/logout-icon.svg';
+import { useNavigate } from 'react-router-dom';
 
 
 function Header() {
+    const navigate = useNavigate();
+
+    function handleLogOut() {
+        navigate('/');
+    }
+
+
+
     return (
         <header>
-            <img src={Logo} alt='logo' />
-            <div className='container-log-out'>
-                <div className='profile-area'>
-                    <img src={Profile} alt='profile' />
-                    <strong>NOME AQUI</strong>
+            <div className='width-limit content-header'>
+                <img src={Logo} alt='logo' />
+                <div className='container-log-out'>
+                    <div className='profile-area'>
+                        <img src={Profile} alt='profile' />
+                        <strong>NOME AQUI</strong>
+                    </div>
+                    <img
+                        src={LogOut}
+                        alt='logout'
+                        className='log-out'
+                        onClick={handleLogOut}
+                    />
                 </div>
-                <img src={LogOut} alt='logout' className='log-out' />
             </div>
         </header>
     )
