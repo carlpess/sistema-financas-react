801405b00372ce8d498999bbb18678cda1b8044e
Carlos Pessoa <carlos.henriquepessoa15@gmail.com>
2023-03-15 13:53:30 -0300

    função de atualizar dados do usuario integrada com a api

--- src/components/ProfileModal/index.js
+++ src/components/ProfileModal/index.js
@@ -1,7 +1,79 @@
 import './style.css';
 import CloseIcon from '../../assets/close-icon.svg';
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+import { getItem, setItem } from '../../utils/storage';
+
+const defaultForm = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
 
 function ProfileModal({ open, handleClose }) {
+    const token = getItem('token');
+    const [form, setForm] = useState({ ...defaultForm });
+
+    function handleChangeForm({ target }) {
+        setForm({ ...form, [target.name]: target.value })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            if (form.password !== form.confirmPassword) {
+                return;
+            }
+
+            const response = await api.patch('/usuario', {
+                nome: form.name,
+                email: form.email,
+                senha: form.password
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            console.log(response.data);
+
+            setItem('userName', form.name);
+
+            handleClose();
+            setForm({ ...defaultForm });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        async function loadUserProfile() {
+            try {
+                const response = await api.get('/usuario', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+
+                const { nome, email } = response.data;
+
+                setForm({
+                    ...form,
+                    name: nome,
+                    email,
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        if (open) {
+            loadUserProfile();
+        }
+    }, [open])
+
     return (
         <>
             {open &&
@@ -15,22 +87,46 @@ function ProfileModal({ open, handleClose }) {
                         />
                         <h2>Editar Perfil</h2>
 
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className='container-inputs'>
                                 <label>Nome</label>
-                                <input type='text' />
+                                <input
+                                    name='name'
+                                    type='text'
+                                    value={form.name}
+                                    onChange={handleChangeForm}
+                                    required
+                                />
                             </div>
                             <div className='container-inputs'>
                                 <label>E-mail</label>
-                                <input type='email' />
+                                <input
+                                    name='email'
+                                    type='email'
+                                    value={form.email}
+                                    onChange={handleChangeForm}
+                                    required
+                                />
                             </div>
                             <div className='container-inputs'>
                                 <label>Senha</label>
-                                <input type='password' />
+                                <input
+                                    name='password'
+                                    type='password'
+                                    value={form.password}
+                                    onChange={handleChangeForm}
+                                    required
+                                />
                             </div>
                             <div className='container-inputs'>
                                 <label>Confirmação de Senha</label>
-                                <input type='password' />
+                                <input
+                                    name='confirmPassword'
+                                    type='password'
+                                    value={form.confirmPassword}
+                                    onChange={handleChangeForm}
+                                    required
+                                />
                             </div>
 
                             <button className='btn-purple btn-medium'>Confirmar</button>
